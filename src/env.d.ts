1c7d7b8ab051f34bb7f2cb390d72beed1f4920fb
contractorservices <contractorservicesagency@gmail.com>
2026-06-24 21:00:48 -0500

    Feat: integrated instant email notification alerts via Resend

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="astro/client" />
 /// <reference types="@cloudflare/workers-types" />
 /// <reference path="../worker-configuration.d.ts" />
+
+interface Env {
+  RESEND_API_KEY: string;
+}
